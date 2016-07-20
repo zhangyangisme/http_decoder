e99ba9e39c6ea368eedcdc794f9d97349224496f
zhangyang <173231752@qq.com>
2016-07-20 23:28:47 +0800

    first commit

---
+++ http_decoder.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+
+import os
+import struct
+import StringIO
+
+
+
+class Packet(object):
+    def __init__(self,buff):
+        self.orig_data = buff
+        self._l3_proto = 0
+        self._l4_proto = 0
+        self._l3_offset = 0
+        self._l4_offset = 0
+        self._l7_offset = 0
+        self._drop = 0
+        self._tunple = (,)
+        return
+    def __parse_l3(self):
+       self._tunple[0] = 
+       self._tunple[1] = 
+       return 
+    @property
+    def drop(self):
+        return self._drop
+    @property
+    def l3_proto(self):
+        return self._l3_proto
+    @property
+    def l4_proto(self):
+        return self._l4_proto
+    @property
+    def tunple(self):
+        return self._tunple
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
