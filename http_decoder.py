96d80cb61956b19e2dfecdd15cfcaf552e0657f0
zy <zy@localhost.localdomain>
2016-07-24 16:31:59 +0800

    111

--- http_decoder.py
+++ http_decoder.py
@@ -2,11 +2,27 @@
 
 
 import os
+import sys
+import getopt
 import struct
 import StringIO
 
+def usage():
+    print ("./http_decoder.py -r <input_file>")
 
 def main():
+    opts,args = getopt.getopt(sys.argv[1:],"r:")
+    input_file = ""
+    if len(sys.argv) != 2:
+        usage()
+        exit()
+    for op,value in opts:
+        if op == "-r":
+            input_file = value
+            print input_file
+        else:
+            usage()
+            exit()
     return 0
 
 
